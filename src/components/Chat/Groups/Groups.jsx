f43f5ba027d6ab6511b2f2a16381fb3d534eff5f
novittana <novittana2410@gmail.com>
2023-11-16 23:52:15 +0200

    changed Groups

--- src/components/Chat/Groups/Groups.jsx
+++ src/components/Chat/Groups/Groups.jsx
@@ -3,8 +3,10 @@ import axios from "axios";
 import {setConversations} from "../../../redux/conversation/conversationSlice";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect, useState} from "react";
+import TimeAgo from "react-timeago";
+import Conversation from "../Messages/Conversation";
 
-function Groups () {
+function Groups() {
     console.log("groups")
     const [groups, setGroups] = useState([]);
     const user = useSelector(state => state.userData.user);
@@ -19,11 +21,32 @@ function Groups () {
         }
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         getAllGroups();
-    },[user])
+    }, [user])
 
     console.log(groups);
+
+
+    return <>
+        <ul>
+            {
+            groups.map((gr) => (
+                <li key={gr._id}>
+                    {/*<img/>*/}
+                    <div className="chat_list_info_wrapper">
+                        <div className="chat_list_item_wrapper">
+                            <h3 className="chat_list_item_username">{gr.name}</h3>
+                            <div><TimeAgo date={gr.createdAt}/></div>
+                        </div>
+                        <p>Message</p>
+                    </div>
+                </li>
+            ))
+        }
+
+        </ul>
+    </>
 };
 
 
